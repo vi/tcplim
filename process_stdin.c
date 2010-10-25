662c4bcf10f4a7dcc4e21ac6828cbccf20626f51
Vitaly _Vi Shukela <public_vi@tut.by>
2010-10-25 16:13:01 +0300

    Improve stdin control interface for tcplim

--- process_stdin.c
+++ process_stdin.c
@@ -10,7 +10,7 @@ static void list_connections() {
 
 	int peerfd = fdinfo[fd].peerfd;
 	
-	printf("    %s:%d -> %s:%d [%d->%d] %lld:%lld\n",
+	printf("    %s:%d -> %s:%d [%d->%d] %lld:%lld limit: %d:%d\n",
 		inet_ntoa(fdinfo[fd].address.sin_addr),
 	       ntohs(fdinfo[fd].address.sin_port),
 	       inet_ntoa(fdinfo[peerfd].address.sin_addr),
@@ -18,7 +18,9 @@ static void list_connections() {
 	       fd, 
 	       peerfd,
 	       fdinfo[fd].total_read,
-	       fdinfo[peerfd].total_read);
+	       fdinfo[peerfd].total_read,
+	       fdinfo[fd].speed_limit,
+	       fdinfo[peerfd].speed_limit);
     }
 }
 
@@ -26,7 +28,8 @@ static void process_stdin() {
     dpf("processing stdin\n");
     char cmd[256]={0};
     char arg[256]={0};
-    scanf("%256s%256s", cmd, arg); 
+    char arg2[256]={0};
+    scanf("%256s%256s%256s", cmd, arg, arg2); 
     dpf("    c=\"%s\" arg=\"%s\"\n", cmd, arg);
     switch(cmd[0]) {
 	case 'q':
@@ -42,9 +45,26 @@ static void process_stdin() {
 		close_fd(atoi(arg));
 	    }
 	    break;
+	case 't':
+	    if(!arg[0]) {
+                printf("timetick number\n");
+	    } else {
+		timetick = (atoi(arg));
+	    }
+	    break;
+	case 'i':
+	    if(!arg[0] || !arg2[0]) {
+                printf("i fd_number new_limit\n");
+	    } else {
+		int fd = (atoi(arg));
+		int new_limit = atoi(arg2);
+		fdinfo[fd].speed_limit = new_limit;
+		quotas_are_full=0;
+	    }
+	    break;
 	case '\0':
 	    break;
 	default:
-	    printf("Commands: quit list kill\n");
+	    printf("Commands: quit list kill timetick lImit\n");
     }
 }
